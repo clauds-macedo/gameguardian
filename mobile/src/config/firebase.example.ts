60980e5b6081658f4ffdd5922d91ae3022bd4762
clauds-macedo <yanalogic@gmail.com>
2023-09-01 19:49:27 -0300

    feat: adiciona arquivo example do firebase

---
+++ mobile/src/config/firebase.example.ts
@@ -0,0 +1,13 @@
+import firebase from '@react-native-firebase/app';
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+export {firebase, firebaseConfig};
