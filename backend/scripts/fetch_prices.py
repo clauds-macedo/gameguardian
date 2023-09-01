5c7b28a6203d66a81eea63fe53f7409004045399
clauds-macedo <yanalogic@gmail.com>
2023-09-01 00:21:16 -0300

    refactor: refatora função e a torna adaptável a depender do tipo da response

--- backend/scripts/fetch_prices.py
+++ backend/scripts/fetch_prices.py
@@ -1,35 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-
-from find_games import find_games
-
-import configs
+import re
 
 def fetch_prices(platform, page_num=1):
-    platforms = configs.platforms
-    if platform not in platforms:
-        return f"Plataforma {platform} não é suportada."
-
-    config = platforms[platform]
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-
-    if "pagination" in config:
-        pagination = config["pagination"]
-        start_index = int(page_num) * int(pagination["page_size"])
-        url = f"{pagination['base_url']}&{pagination['start_param']}={start_index}"
-    else:
-        url = config["url"]
-    if platform == "steam":
-        url = url + "&specials=1"
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        return "Erro ao acessar página."
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    promo_games = soup.findAll(config["class"]["name"], config["class"]["attrs"])
+    response = requests.get("https://store.steampowered.com/search/results/?query&start=50&count=300&dynamic_data=&sort_by=_ASC&ignore_preferences=1&supportedlang=portuguese&snr=1_7_7_2300_7&specials=1&infinite=1")
     
-    return find_games(promo_games, config)
+    content_type = response.headers.get('Content-Type')
+    app_id_pattern = re.compile(r'/app/(\d+)')
+    
+    if "html" in content_type:
+        soup = BeautifulSoup(response.content, "html.parser")
+        promo_games_with_app = [promo for promo in soup.findAll("a", href=True) if "app" in promo["href"]]
+        id_list = []
+        for promo in promo_games_with_app:
+            match = app_id_pattern.search(promo["href"])
+            if match:
+                app_id = match.group(1)
+                id_list.append(app_id)
+        print(id_list)
+
+    elif "json" in content_type:
+        data = response.json()
+        if 'results_html' in data:
+            soup = BeautifulSoup(data['results_html'], "html.parser")
+            
+            promo_games_with_app = [promo for promo in soup.findAll("a", href=True) if "app" in promo["href"]]
+            id_list = []
+            for promo in promo_games_with_app:
+                match = app_id_pattern.search(promo["href"])
+                if match:
+                    app_id = match.group(1)
+                    id_list.append(app_id)
+            print(id_list)
+
+fetch_prices("steam")
