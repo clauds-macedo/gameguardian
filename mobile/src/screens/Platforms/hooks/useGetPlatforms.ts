5c8af78be7eeef4a5e121f8026c25631bfddc969
clauds-macedo <yanalogic@gmail.com>
2024-05-31 22:50:04 -0300

    chore: remove logs

---
+++ mobile/src/screens/Platforms/hooks/useGetPlatforms.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+import { remoteConfigUseCase } from '../../../data/usecases/remoteConfigUseCase';
+
+export interface PlatformInfo {
+  name: string;
+  src: string;
+}
+
+const useGetPlatforms = () => {
+  const [platforms, setPlatforms] = useState<PlatformInfo[]>(
+    [] as PlatformInfo[]
+  );
+
+  const getPlatforms = useCallback(async () => {
+    await remoteConfigUseCase.establishRemoteConfigConnection();
+    const value = await remoteConfigUseCase.getValue('platforms');
+    if (value) {
+      setPlatforms(value.asArray() as PlatformInfo[]);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPlatforms();
+  }, [getPlatforms]);
+
+  return { platforms };
+};
+
+export default useGetPlatforms;
